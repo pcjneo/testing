0ddf57c42b903858fc9fc4c3d29ed83931282c9f
pcjneo <46348577+pcjneo@users.noreply.github.com>
2019-01-03 19:18:27 +0530

    Add files via upload

---
+++ JenkinDemo.java
@@ -0,0 +1,12 @@
+
+public class JenkinDemo {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Hello from Jenkins");
+       System.out.println("Testing  from Jenkins");
+       System.out.println("Bye from Jenkins");
+	}
+
+}
